00e80f755663bdd771c9d06b69f9a359594b49e9
styley997 <styley997@gmail.com>
2021-12-27 20:04:05 +0700

    initial project

--- labex_frontend/src/main.js
+++ labex_frontend/src/main.js
@@ -1,9 +1,9 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
+import store from './store'
+import axios from 'axios'
 
-const myApp = createApp(App)
-myApp.use(router)
-myApp.mount('#app')
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+
+createApp(App).use(store).use(router, axios).mount('#app')
--- labex_frontend/src/router/index.js
+++ labex_frontend/src/router/index.js
@@ -1,23 +1,55 @@
-import { createWebHistory, createRouter } from "vue-router";
-import Home from "@/components/Home.vue";
-// import About from "@/views/About.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Dashboard from '../views/Dashboard.vue'
+import SignUp from '../views/SignUp.vue'
+import LogIn from '../views/LogIn.vue'
+import store from '../store'
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: Home,
+    path: '/',
+    name: 'Home',
+    component: Home
   },
-//   {
-//     path: "/about",
-//     name: "About",
-//     component: About,
-//   },
-];
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/log-in',
+    name: 'LogIn',
+    component: LogIn
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
+    meta: {
+      requireLogin: true
+    }
+  },
+]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
 
-export default router;
+router.beforeEach((to, from, next) => {
+  if(to.matched.some(record => record.meta.requireLogin) && !store.state.isAuthenticated) {
+    next('/log-in')
+  } else {
+    next()
+  }
+})
+
+export default router
